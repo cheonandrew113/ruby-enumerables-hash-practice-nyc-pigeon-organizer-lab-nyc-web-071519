1db16218b59adaa762602741fd72f6b9c12f8c77
Andnrew Cheon <cheonandrew113@gmail.com>
2019-07-12 15:24:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -38,3 +38,19 @@ pigeon_list = {
 }
 
 end
+
+pigeon_data = {
+  :color => {
+    :brown => ["Luca"],
+    :black => ["Lola"],
+  },
+  :gender => {
+    :male => ["Luca"],
+    :female => ["Lola"]
+  },
+  :lives => {
+    "Central Park" => ["Lola"],
+    "Library" => ["Luca"]
+  }
+}
+end
